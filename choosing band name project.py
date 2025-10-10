147b01e9ab2aa666fb55abfeb34ec93ad92efdfd
Mayfredrick <108125365+Mayfredrick@users.noreply.github.com>
2025-10-10 05:30:49 -0400

    Update choosing band name project.py

--- choosing band name project.py
+++ choosing band name project.py
@@ -1,5 +1,6 @@
 #Simple python project to choose the band name based on the city and pet name
-city = input("Please enter the city where you where born:\n")
+city = input("Please enter the city where you were born:\n")
 pet_name = input("please enter your favorate name of your pet:\n")
+
 
 print(" Your band name choosen is"+ " " + city+" "+ pet_name + ", cool right!")
