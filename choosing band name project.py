7c1ce98ba59c95956b43e21832a697d7c2f39678
Mayfredrick <108125365+Mayfredrick@users.noreply.github.com>
2025-10-10 05:29:35 -0400

    Add files via upload
    This is the simple python project which will give out the output of your band based on the two inputs. That is city name and the pet name.

---
+++ choosing band name project.py
@@ -0,0 +1,5 @@
+#Simple python project to choose the band name based on the city and pet name
+city = input("Please enter the city where you where born:\n")
+pet_name = input("please enter your favorate name of your pet:\n")
+
+print(" Your band name choosen is"+ " " + city+" "+ pet_name + ", cool right!")
